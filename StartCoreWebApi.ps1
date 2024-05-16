aa429ff434e0dab6f38a16a00b44dead3cff5087
Zijian <161275903+ZijianFLG@users.noreply.github.com>
2024-05-17 06:48:21 +1000

    localhost

--- StartCoreWebApi.ps1
+++ StartCoreWebApi.ps1
@@ -8,4 +8,4 @@ $procArgs = @{
 }
 $process = Start-Process @procArgs
 
-Invoke-RestMethod http://127.0.0.1:5000/WeatherForecast -Method GET
+Invoke-RestMethod http://localhost:5000/WeatherForecast -Method GET
