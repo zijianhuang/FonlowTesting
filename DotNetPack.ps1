90acad8b75af442726460d2d2dc8ce791e5134cf
Z <zijianhuang2000@gmail.com>
2025-07-16 18:28:29 +1000

    ok

--- DotNetPack.ps1
+++ DotNetPack.ps1
@@ -1,5 +1,5 @@
-$packCmd = 'dotnet pack $Name --no-build --output C:/NugetLocalFeeds --configuration Release'
 $projList = 'Fonlow.Testing.HttpCore/Fonlow.Testing.HttpCore.csproj', 'Fonlow.Testing.ServiceCore/Fonlow.Testing.ServiceCore.csproj', 'Fonlow.Testing.Basic/Fonlow.Testing.Basic.csproj', 'Fonlow.Testing.Integration/Fonlow.Testing.Integration.csproj', 'Fonlow.Testing.Utilities/Fonlow.Testing.Utilities.csproj'
 foreach($name in $projList){
+    $packCmd = 'dotnet pack $name --no-build --output C:/NugetLocalFeeds --configuration Release'
     Invoke-Expression $ExecutionContext.InvokeCommand.ExpandString($packCmd)
 }
